3a9faba5de067f921e39af65b4ecc02848addd13
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-11-19 22:29:44 -0600

    improvments into models

--- bmx/series/models.py
+++ bmx/series/models.py
@@ -8,7 +8,7 @@ from .managers import UdiManager, DollarManager
 
 
 class Udi(models.Model):
-    date = models.DateField(_('date'))
+    date = models.DateField(_('date'), unique=True)
     value = models.FloatField(_('value'))
 
     objects = UdiManager()
@@ -16,17 +16,13 @@ class Udi(models.Model):
     class Meta:
         verbose_name = _('udi')
         verbose_name_plural = _('udis')
-        constraints = [
-            models.UniqueConstraint(
-                fields=['date', 'value'], name='udi_date_value_unique')
-        ]
 
     def __str__(self):
         return "%s %s" % (self.date, self.value)
 
 
 class Dollar(models.Model):
-    date = models.DateField(_('date'))
+    date = models.DateField(_('date'), unique=True)
     value = models.FloatField(_('value'))
 
     objects = DollarManager()
@@ -34,10 +30,6 @@ class Dollar(models.Model):
     class Meta:
         verbose_name = _('dollar')
         verbose_name_plural = _('dollars')
-        constraints = [
-            models.UniqueConstraint(
-                fields=['date', 'value'], name='dollar_date_value_unique')
-        ]
 
     def __str__(self):
         return "%s %s" % (self.date, self.value)
