4aee2a5a74c08e902d1d91783ccea20d5888c36b
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-11-19 21:04:43 -0600

    add data models

--- bmx/series/models.py
+++ bmx/series/models.py
@@ -2,29 +2,44 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+from .managers import UdiManager, DollarManager
+
 # Create your models here.
+
 
 class Udi(models.Model):
     date = models.DateField(_('date'))
     value = models.FloatField(_('value'))
 
+    objects = UdiManager()
+
     class Meta:
         verbose_name = _('udi')
         verbose_name_plural = _('udis')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['date', 'value'], name='udi_date_value_unique')
+        ]
 
     def __str__(self):
-        return self.value
+        return "%s %s" % (self.date, self.value)
 
 
 class Dollar(models.Model):
     date = models.DateField(_('date'))
     value = models.FloatField(_('value'))
 
+    objects = DollarManager()
+
     class Meta:
         verbose_name = _('dollar')
         verbose_name_plural = _('dollars')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['date', 'value'], name='dollar_date_value_unique')
+        ]
 
     def __str__(self):
-        return self.value
+        return "%s %s" % (self.date, self.value)
 
 # TODO: TIIE
